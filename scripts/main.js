c89ddaba21ecd49a17e2ede4d0904c2439f0f5ec
Boris <478635054@qq.com>
2021-06-02 15:34:34 +0800

    update

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+let myHeading = document.querySelector("#Welcomemessage");
+myHeading.textContent = 'Hello world!';
+/*function setUsername()
+{
+    let Username = prompt("I appreciate if you can tell my name before leaving comments")
+    if (!Username || Username === null)
+    {
+        setUsername();
+    }
+    else
+    {
+
+    }
+}*/
