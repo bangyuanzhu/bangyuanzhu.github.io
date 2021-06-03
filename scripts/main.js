fbd8d05e656be52b90736374767e510caeb21e29
Boris <478635054@qq.com>
2021-06-03 10:00:59 +0800

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,31 @@
-let myHeading = document.querySelector("#Welcomemessage");
-myHeading.textContent = 'Hello world!';
-/*function setUsername()
+let myHeading = document.querySelector('#Welcomemessage');
+let myButton = document.querySelector('button');
+
+function setUsername()
 {
-    let Username = prompt("I appreciate if you can tell my name before leaving comments")
+    let Username = prompt("I appreciate if you can tell your name before leaving comments")
     if (!Username || Username === null)
     {
         setUsername();
     }
     else
     {
-
+        localStorage.setItem('name', Username);
+        myHeading.innerHTML = "Welcome "+ Username + " to my blog: to record my life and work";
     }
-}*/
+}
+
+if(!localStorage.getItem('name'))
+{
+    setUsername();
+}
+else
+{
+    let Storedname = localStorage.getItem('name');
+    myHeading.textContent = "Welcome "+ Storedname + " to my blog: to record my life and work";
+}
+
+myButton.onclick = function()
+{
+    setUsername();
+}
